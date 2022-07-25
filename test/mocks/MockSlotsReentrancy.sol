4c233a2cb911ec9e049572bff00188fa5e2dd8a3
nannafudge <6781691+nannafudge@users.noreply.github.com>
2022-07-25 23:36:46 +0100

    Updating Reentrancy contract to only call withdrawBet once to remove any erronous test outcomes.

--- test/mocks/MockSlotsReentrancy.sol
+++ test/mocks/MockSlotsReentrancy.sol
@@ -5,11 +5,23 @@ import "src/BasicVideoSlots.sol";
 
 // Contract that attemps to withdraw its bet during the payout
 contract MockSlotsReentrancy {
+    // Called ensures this contract only calls withdrawBet once.
+    // Otherwise the call to pay the malicious contract in withdrawBet
+    // could trigger the reentrancy guard and cause the test to pass, even
+    // though the call to withdrawBet did succeed
+    bool private called = false;
+
     receive() external payable {
-        BasicVideoSlots(msg.sender).withdrawBet();
+        if (!called) {
+            BasicVideoSlots(msg.sender).withdrawBet();
+            called = true;
+        }
     }
 
     fallback() external payable {
-        BasicVideoSlots(msg.sender).withdrawBet();
+        if (!called) {
+            BasicVideoSlots(msg.sender).withdrawBet();
+            called = true;
+        }
     }
 }
